a47e35cc011eabc1a87829c857f8f6e41e04c4e6
trcarneiro <thiago@altostratus.com.br>
2025-11-25 03:21:01 -0300

    fix: use Node moduleResolution and add debug logs to api entry point

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,13 @@
-import app from '../server/index'
+// Vercel Serverless Function Entry Point
+console.log('[API] Loading api/index.ts...')
+
+try {
+  // @ts-ignore
+  var app = require('../server/index').default
+  console.log('[API] Successfully imported app from ../server/index')
+} catch (e) {
+  console.error('[API] Failed to import app:', e)
+  throw e
+}
 
 export default app
